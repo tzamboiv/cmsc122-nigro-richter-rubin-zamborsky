53d80626c326433874cd6496285b4d9be93b740e
Teddy Zamborsky <tzamborsky@cs.uchicago.edu>
2018-03-07 15:34:43 -0600

    Finished basic input screen django

--- django/TransportationRating/mysite/travelprefs/models.py
+++ django/TransportationRating/mysite/travelprefs/models.py
@@ -9,3 +9,17 @@ class Preferences(models.Model):
 	transitpref = models.CharField(max_length=500)
 	destpref = models.CharField(max_length=500)
 	interests = models.TextField()
+
+class POI(models.Model):
+	poi_ad = models.TextField()
+
+class All(models.Model):
+	streetad = models.TextField(default = "none")
+	poi = models.TextField(default = "none")
+	divvy = models.TextField(default = "none")
+	cta = models.TextField(default = "none")
+	shuttles = models.TextField(default = "none")
+	driving = models.TextField(default = "none")
+	bicycling = models.TextField(default = "none")
+	walking = models.TextField(default = "none")
+
