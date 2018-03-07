facd020a361604a13d3f2614497a8684564d9c55
devanrichter <devanrichter@uchicago.edu>
2018-03-06 21:02:25 -0600

    Interface Update
    Change Landing Page

--- django/TransportationRating/mysite/mysite/urls.py
+++ django/TransportationRating/mysite/mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('preferences/', include('travelprefs.urls')),
+    path('', include('travelprefs.urls')),
 ]
