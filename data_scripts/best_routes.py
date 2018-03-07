d1f74caf22a8bf0bd9d499aa9da4276d8baf44dc
Nick Nigro <nick@Nicks-MacBook.local>
2018-03-06 19:41:29 -0600

    non-duplicates

---
+++ data_scripts/best_routes.py
@@ -0,0 +1,34 @@
+from points_of_interest import time_to_dest
+import math
+
+WEIGHTS = {
+    "low": 2,
+    "medium": 1,
+    "high": 0.5
+}
+
+def best_travel_options(address_1, address_2, transit_inputs):
+    travel_times = time_to_dest(adress_1, address_2, transit_inputs)
+    adjusted_times = [WEIGHTS[type] * travel_times["type"] for type in travel_times]
+    best_time = math.inf
+    second_time = math.inf
+    best = None
+    second = None
+    for transit_type, time in adjusted_times.items():
+        if time < best_time:
+            second_time = best
+            second = best
+            best_time = time
+            best = transit_type
+        elif time < second_time:
+            second_time = time
+            second = transit_type
+    return [("best", "best_time"), ("second", "second_time")]
+
+
+def compute_initial_score(address_1, address_2, transit_inputs):
+    best_options = best_travel_options(address_1, address_2, transit_inputs)
+    # Mainly a placeholder- want to take into account both best and second best options
+    # reward best option for being good and penalize second best for being bad
+    total_score = math.log(best_options[0][1]) + 0.5 * math.exp(best_options[1][1])
+    return total_score
